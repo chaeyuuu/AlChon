98574cbcfb8c3dd3a6b89ecb3e3bfdacb81a6315
yeonjy73 <yeon703@naver.com>
2025-03-11 10:35:46 +0900

    24460 recursion error

---
+++ Recursion&Backtracking/박연주/24460.py
@@ -0,0 +1,35 @@
+def recursion(input_list, lineNum):
+    # 찾으려는 구역의 크기가 4면 두번째로 작은 수
+    if lineNum == 2:
+        flat_list = [j for i in input_list for j in i]
+        flat_list.sort()
+        return flat_list[1]
+    
+    n = lineNum//2
+    
+    searchList1 = [i[:n] for i in input_list[:n]]
+    searchList2 = [i[n:] for i in input_list[:n]]
+    searchList3 = [i[:n] for i in input_list[n:]]
+    searchList4 = [i[n:] for i in input_list[n:]]
+
+    findList = []
+    findList.append(recursion(searchList1, n))
+    findList.append(recursion(searchList2, n))
+    findList.append(recursion(searchList3, n))
+    findList.append(recursion(searchList4, n))
+    
+    findList.sort()
+    return findList[1]
+
+lineNum = int(input())
+input_list = []
+
+if lineNum == 0:
+    print(input())
+    exit(0)
+else:
+    for i in range(lineNum):
+        line = [int(j) for j in input().split()]
+        input_list.append(line)
+
+print(recursion(input_list, lineNum))
