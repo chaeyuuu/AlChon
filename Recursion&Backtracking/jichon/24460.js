4679a22806e775f22510fd54c5660b38852fd964
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-03-13 16:34:21 +0900

    fix: fix 24460

--- Recursion&Backtracking/jichon/24460.js
+++ Recursion&Backtracking/jichon/24460.js
@@ -1,46 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-const recur = function (lotto, n) {
-  if (n == 2) {
-    let flat_lotto = lotto.toString().split(",").map(Number);
-    flat_lotto.sort((a, b) => a - b);
-    return flat_lotto[1];
-  }
+function recur(lotto, n) {
+  if (n === 1) return lotto[0][0]; //그룹 중 제일 작은 거
 
-  n = n / 2;
-  let lotto_1 = Array.from({ length: n }, () => Array(n).fill(0));
-  let lotto_2 = Array.from({ length: n }, () => Array(n).fill(0));
-  let lotto_3 = Array.from({ length: n }, () => Array(n).fill(0));
-  let lotto_4 = Array.from({ length: n }, () => Array(n).fill(0));
+  let mid = Math.floor(n / 2);
+  let quadrants = [
+    lotto.slice(0, mid).map((row) => row.slice(0, mid)),
+    lotto.slice(0, mid).map((row) => row.slice(mid)),
+    lotto.slice(mid).map((row) => row.slice(0, mid)),
+    lotto.slice(mid).map((row) => row.slice(mid)),
+  ];
+  let winners = quadrants.map((q) => recur(q, mid));
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      lotto_1[j][i] = lotto[j][i];
-      lotto_2[j][i] = lotto[j + n][i];
-      lotto_3[j][i] = lotto[j][i + n];
-      lotto_4[j][i] = lotto[j + n][i + n];
-    }
-  }
-
-  let result_list = [];
-  result_list.push(recur(lotto_1, n));
-  result_list.push(recur(lotto_2, n));
-  result_list.push(recur(lotto_3, n));
-  result_list.push(recur(lotto_4, n));
-
-  result_list.sort();
-  return result_list[1];
-};
+  winners.sort((a, b) => a - b);
+  return winners[1];
+}
 
 const solution = () => {
   let n = +input[0];
-  let lotto = Array.from({ length: n }, () => Array(n).fill(0));
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      lotto[j][i] = +input[j + 1].split(" ")[i];
-    }
+  let lotto = [];
+
+  for (let i = 1; i <= n; i++) {
+    lotto.push(input[i].split(" ").map(Number));
   }
+
   console.log(recur(lotto, n));
 };
 
