68a3b4be203acd03c4fb31c40c114a1517469e2c
Chae-Yu <chaeyu2004@naver.com>
2025-03-17 16:04:04 +0900

    error: boj.2121 "넷이 놀기"
    - 시간 초과 뜸

---
+++ Binary Search/chaechon/2121.py
@@ -0,0 +1,22 @@
+# 직사각형 조건
+# 1. 마주보는 두 변의 길이가 같음
+# .. 
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+nlist, temp = [], []
+result=0
+
+a,b = map(int,input().split())
+
+for _ in range(N):
+    x, y = map(int,input().split())
+    nlist.append((x,y))
+    
+for x,y in nlist:
+    if (x+a, y) in nlist and (x, y+b) in nlist and (x+a, y+b) in nlist:
+        result += 1
+    
+print(result)
