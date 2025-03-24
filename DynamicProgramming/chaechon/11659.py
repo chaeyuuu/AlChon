493d862555a675e383cab119b9230ba7992f0afb
Chae-Yu <chaeyu2004@naver.com>
2025-03-24 16:09:10 +0900

    code: boj.11659 "구간 합 구하기 4"

--- DynamicProgramming/chaechon/11659.py
+++ DynamicProgramming/chaechon/11659.py
@@ -3,19 +3,26 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int,input().split())
-print(N, M)
+# print(N, M)
 
 num_list = list(map(int, input().split()))
-print(num_list)
+dp = [0] * (N)
+dp[0] = num_list[0]
+# print(dp)
+# print(num_list)
+
+for i in range(1, N):
+    dp[i] = dp[i-1] + num_list[i] 
+# print(dp)
 
 for _ in range(M):
-    # print_list.append((int, input().split()))
     i, j = (map(int, input().split()))
-    print(i, j)
-    
-    result = 0
-
-    for num in range(i-1, j):
-        result += num_list[num]
-    
-    print(result)
+    # print(i, j)
+        
+    if i==j:
+        print(num_list[i-1])
+    else:
+        if i==1:
+            print(dp[j-1])
+        else:
+            print(dp[j-1]-dp[i-2])
