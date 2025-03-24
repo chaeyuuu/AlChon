8a4a9f483d5fd9e0d8c13e2c727e1aa810878f5c
Chae-Yu <chaeyu2004@naver.com>
2025-03-24 15:24:00 +0900

    code: boj.1463 "1로 만들기"
    - dp 너무 어려워

--- DynamicProgramming/1463.py
+++ DynamicProgramming/1463.py
@@ -6,9 +6,9 @@ dp = [0] * (N+1)
 
 for i in range(2, N+1):
     dp[i] = dp[i-1] + 1
-    if N%3 == 0:
+    if i%3 == 0:
         dp[i] = min(dp[i//3]+1, dp[i])
-    if N%2 ==0:
+    if i%2 ==0:
         dp[i] = min(dp[i//2]+1, dp[i])
 
 print(dp[N])
