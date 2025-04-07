785acd234a2ed0bf51830b96ba6555744b70a83e
yeonjy73 <yeon703@naver.com>
2025-04-07 20:55:01 +0900

    docs: add 11047 ver2

--- Greedy/yeonchon/11047.py
+++ Greedy/yeonchon/11047.py
@@ -5,14 +5,13 @@ n, k = map(int, reader().strip().split())
 
 coinList = [int(reader().strip()) for _ in range(n)]
     
-coinList.sort(reverse=True)
 count = 0
 
-for coin in coinList: 
+for i in range(n-1, -1, -1): # 역순순회: n-1부터 0까지 -1 step으로 순회
     if k == 0:
         break
-    if k >= coin:
-        count += k // coin
-        k %= coin
+    if k >= coinList[i]:
+        count += k // coinList[i]
+        k %= coinList[i]
         
 print(count)
