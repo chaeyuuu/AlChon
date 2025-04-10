5326c0ff305fef97f8b92cddc1a020b8152c0246
yeonjy73 <yeon703@naver.com>
2025-04-10 10:28:58 +0900

    chore: 주석추가 잘못이해했는데 풀림 ㅙ지?

--- Greedy/yeonchon/1541.py
+++ Greedy/yeonchon/1541.py
@@ -1,3 +1,8 @@
+# 3 + 4 - 3 - 10 + 24 
+# (3 + 4) - (3) - (10 + 24) = -30 
+# - 사이사이에 있는 +들을 더 더해서 크기를 키우고 -로 부호를 만들어줌
+# - 로 split 하고 사이사이를 다 더해서 빼줌
+
 import sys
 reader = sys.stdin.readline
 
@@ -9,8 +14,8 @@ result = 0
 for exp in input[:1]:
     result += sum(map(int, exp.split("+"))) # sum함수: iterable요소를 모두 더해줌
     
-# 첫 - 뒤 쪽 모두 계산
+# - 로 자른 부분식 더해서 -로 묶어서 빼기
 for exp in input[1:]:
-    result -= sum(map(int, exp.split("+"))) # -로 시작하는 거는 빼는게 아니라 -40을 더한다고 생각
+    result -= sum(map(int, exp.split("+")))
         
 print(result)
