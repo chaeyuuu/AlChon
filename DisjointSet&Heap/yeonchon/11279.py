d73314853e20344f318a0090b26ad83b36bb4953
yeonjy73 <yeon703@naver.com>
2025-05-13 23:44:04 +0900

    docs: add 11279

--- DisjointSet&Heap/yeonchon/11279.py
+++ DisjointSet&Heap/yeonchon/11279.py
@@ -4,66 +4,23 @@
 # 부모 노드의 인덱스 = 자식 노드의 인덱스 / 2
 
 import sys
+import heapq
 input = sys.stdin.readline
     
 
 num = int(input().strip())
-priorityQueue = [0] * (num+ 1 )# 0번 인덱스는 사용 X
-size = 0 
+maxHeap = []
 
-for i in range(num):
+for _ in range(num):
     x = int(input().strip())
+    
     if x == 0:
-        if size == 0:
+        if len(maxHeap) == 0:
             print(0)
         else:
-            print(priorityQueue.pop(1))
-            
-            size -= 1
-            index = 1
-            lastNode = priorityQueue.pop(size)
-            priorityQueue.insert(1, lastNode)
-            
-            # heapify (down-heap)
-            while index <= size:
-                left = index*2
-                right = index*2 + 1
-                
-                # 자식 노드가 없을때
-                if left > size: 
-                    break
-                
-                # 왼쪽 자식 노드만 있을때
-                elif left == size: 
-                    if priorityQueue[left] > lastNode:
-                        priorityQueue[index] = priorityQueue[left]
-                        priorityQueue[left] = lastNode
-                        index = left
-                    break
-                
-                # 두 자식 노드 다 있을 때
-                else: # 두 자식 중 하나가 자신보다 클때, 더 큰 자식과 자리를 바꾼다.
-                    if priorityQueue[left] > lastNode or priorityQueue[right] > lastNode:
-                        if priorityQueue[left] > priorityQueue[right]: # 더 큰거랑 바꿔줌
-                            priorityQueue[index] = priorityQueue[left]
-                            priorityQueue[left] = lastNode
-                            index = left
-                        else:
-                            priorityQueue[index] = priorityQueue[right]
-                            priorityQueue[right] = lastNode
-                            index = right
-                    else: # 두 자식이 모두 나보다 작을때
-                        break
-                        
+            print(-heapq.heappop(maxHeap))
             
     else:
-        size += 1
-        index = size
-
-        # heapify (up-heap)
-        while index > 1 and x > priorityQueue[index//2]:
-            priorityQueue[index] = priorityQueue[index//2] # 부모를 밑으로 내림
-            index //= 2
-        priorityQueue[index] = x
+        heapq.heappush(maxHeap, -x)
             
         
