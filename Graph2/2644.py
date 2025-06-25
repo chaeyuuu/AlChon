05c51670bb6d9558f750c77ce6043f69573c7680
yeonjy73 <yeon703@naver.com>
2025-06-26 01:40:23 +0900

    docs: add 2644

--- Graph2/2644.py
+++ Graph2/2644.py
@@ -3,26 +3,21 @@ from collections import deque
 
 input = sys.stdin.readline
 
-def bfs(end, count):
+def bfs(start, end):
     visited = {start}
-    queue = deque([(start, count)])
+    queue = deque([(start, 0)])
     
     while queue:
-        # print(queue, "count = ", count)
-        node = queue.popleft()
-        flag = False
-        # print(node)
+        # print(queue)
+        node, dist = queue.popleft()
         
-        if node[0] == end:
-            return node[1]
+        if node == end:
+            return dist
         
-        for nextNode in graph[node[0]]:
+        for nextNode in graph[node]:
             if nextNode not in visited:
-                flag = True
-                queue.append((nextNode, count+1))
-                visited.add(node[0])
-        if flag:
-            count += 1
+                queue.append((nextNode, dist+1))
+                visited.add(nextNode)
     return -1
         
 n = int(input().rstrip())
@@ -37,5 +32,5 @@ for _ in range(m):
     graph[x].append(y)
     graph[y].append(x) # 양방향
     
-print(bfs(end, 0))
+print(bfs(start, end))
     
