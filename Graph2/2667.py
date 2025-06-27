2d9aaff25d9554438ef5d410821188c20fb925be
yeonjy73 <yeon703@naver.com>
2025-06-27 14:02:39 +0900

    리퓃토링

--- Graph2/2667.py
+++ Graph2/2667.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-def bfs(x, y):    
+def bfs(graph, x, y, n):    
     queue = deque([(x, y)])
     graph[x][y] = 0
     num = 1
@@ -23,25 +23,28 @@ def bfs(x, y):
                     graph[nx][ny] = 0 # 중복 카운트 없게
     
     return num
+
+def main():
+    n = int(input().strip())
+    count = 0
+    output = []
+    graph = []
+
+    for _ in range(n):
+        graph.append([int(char) for char in input().strip()])
+
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] == 1:
+                count += 1
+                output.append(bfs(graph, i, j, n))
+
+    sys.stdout.write(f"{count}\n")
+
+    output.sort()
+    for num in output:
+        sys.stdout.write(f"{num}\n")
+
+    sys.stdout.flush()
     
-n = int(input().strip())
-count = 0
-output = []
-graph = []
-
-for _ in range(n):
-    graph.append([int(char) for char in input().strip()])
-
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            count += 1
-            output.append(bfs(i, j))
-
-sys.stdout.write(f"{count}\n")
-
-output.sort()
-for num in output:
-    sys.stdout.write(f"{num}\n")
-
-sys.stdout.flush()
+main()
